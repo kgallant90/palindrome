b27c6f6bf7be93648eee42b5a11abae92766352a
Kelton Gallant <kgallant90@gmail.com>
2020-07-03 09:52:44 -0400

    Finish working and refactored palindrome method

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+module.exports = Phrase;
+
 // Adds `reverse` to all strings.
 String.prototype.reverse = function() {
   return Array.from(this).reverse().join("");
@@ -9,107 +11,57 @@ function Phrase(content) {
 
   // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.content.toLowerCase();
+    return this.letters().toLowerCase();
+  }
+
+  // Returns the letters in the content.
+  // For example:
+  //   new Phrase("Hello, world!").letters() === "Helloworld"
+  this.letters = function letters() {
+    return (this.content.match(/[a-z]/gi) || []).join("");
   }
 
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
     return this.processedContent() === this.processedContent().reverse();
   }
-
-  //RETURNS A CAPITALIZED PHRASE (NOT IN CODE TUTORIAL)
-  this.louder = function louder() {
-  let processContent = this.content.toUpperCase();
-  return processContent;
-  }
-
-  this.blank = function blank() {
-    return !this.content.match(/^\s+$/) === null;
-  }
-
 }
 
-  // OLD (BUT NOT THE OLDEST) CODE
-  // // Reverses a string.
-  // function reverse(string) {
-  //   return Array.from(string).reverse().join("");
-  // }
-  //
-  // // Defines a Phrase object.
-  // function Phrase(content) {
-  //   this.content = content;
-  //
-  //   this.processor = function processor() {
-  //     return this.content.toLowerCase();
-  //   }
-  //
-  //   // Returns content processed for palindrome testing.
-  //   this.processedContent = function processedContent() {
-  //     return this.processor();
-  //   }
-  //
-  //   // Returns true if the phrase is a palindrome, false otherwise.
-  //   this.palindrome = function palindrome() {
-  //     return this.processedContent() === reverse(this.processedContent());
-  //   }
-  //
-  //   // Returns a capitalized phrase
-  //   this.louder = function louder() {
-  //     let processContent = this.content.toUpperCase();
-  //     return processContent;
-  //   }
-  // }
-  //
-  // // Defines a TranslatedPhrase object.
-  // function TranslatedPhrase(content, translation) {
-  //   this.content = content;
-  //   this.translation = translation;
-  //
-  //   // Returns translation processed for palindrome testing.
-  //   this.processedContent = function processedContent() {
-  //     return this.processor();
-  //   }
-  // }
-  //
-  // TranslatedPhrase.prototype = new Phrase();
+// ------------ See test.js for instructions to check in REPL ------------ //
 
+// ------------ CODE Prior to REFACTOR ------------ //
 
-  // OLD CODE
-  // // Reverses a string.
-  // function reverse(string) {
-  //   return Array.from(string).reverse().join("");
-  // }
-  //
-  // // Defines a Phrase object.
-  // function Phrase(content) {
-  //   this.content = content;
-  //
-  //   // Returns content processed for palindrome testing.
-  //   this.processedContent = function processedContent() {
-  //     return this.content.toLowerCase();
-  //   }
-  //
-  //   // Returns true if the phrase is a palindrome, false otherwise.
-  //   this.palindrome = function palindrome() {
-  //     return this.processedContent() === reverse(this.processedContent());
-  //   }
-  //
-  //   // Returns a capitalized phrase
-  //   this.louder = function louder() {
-  //     let processContent = this.content.toUpperCase();
-  //     return processContent;
-  //   }
-  // }
-  //
-  // // Defines a TranslatedPhrase object.
-  // function TranslatedPhrase(content, translation) {
-  //   this.content = content;
-  //   this.translation = translation;
-  //
-  //   // Returns translation processed for palindrome testing.
-  //   this.processedContent = function processedContent() {
-  //     return this.translation.toLowerCase();
-  //   }
-  // }
-  //
-  // TranslatedPhrase.prototype = new Phrase();
+    // module.exports = Phrase;
+    //
+    // // Adds `reverse` to all strings.
+    // String.prototype.reverse = function() {
+    //   return Array.from(this).reverse().join("");
+    // }
+    //
+    // // Defines a Phrase object.
+    // function Phrase(content) {
+    //   this.content = content;
+    //
+    //   // Returns content processed for palindrome testing.
+    //   this.processedContent = function processedContent() {
+    //     return this.letters().toLowerCase();
+    //   }
+    //
+    //   // Returns the letters in the content.
+    //   // For example:
+    //   //   new Phrase("Hello, world!").letters() === "Helloworld"
+    //   this.letters = function letters() {
+    //     let theLetters = [];
+    //     for (let i = 0; i < this.content.length; i++) {
+    //       if (this.content.charAt(i).match(/[a-zA-Z]/)) {
+    //         theLetters.push(this.content.charAt(i));
+    //       }
+    //     }
+    //     return theLetters.join("");
+    //   }
+    //
+    //   // Returns true if the phrase is a palindrome, false otherwise.
+    //   this.palindrome = function palindrome() {
+    //     return this.processedContent() === this.processedContent().reverse();
+    //   }
+    // }
---
+++ test/test.js
@@ -0,0 +1,49 @@
+let assert = require("assert");
+let Phrase = require("../index.js");
+
+describe("Phrase", function() {
+
+  describe("#palindrome", function() {
+
+    it("should return false for a non-palidrome", function() {
+      let nonPalindrome = new Phrase("apple");
+      assert(!nonPalindrome.palindrome());
+    });
+
+    it("should return true for a plain palindrome", function() {
+      let plainPalindrome = new Phrase("racecar");
+      assert(plainPalindrome.palindrome());
+    });
+    it("should return true for a mixed-case palindrome", function() {
+      let mixedCasePalindrome = new Phrase("RaceCar");
+      assert(mixedCasePalindrome.palindrome());
+    });
+
+    it("should return true for a palindrome with punctuation", function() {
+      let punctuatedPalindrome = new Phrase("Madam, I'm Adam.");
+      assert(punctuatedPalindrome.palindrome());
+    });
+    it("should return true for a palindrome with null elements (i.e. numbers)", function() {
+      let numberedPalindrome = new Phrase("ab123ba");
+      assert(numberedPalindrome.palindrome());
+    });
+
+    describe("#letters", function() {
+      it("should return only letters", function() {
+        let punctuatedPalindrome = new Phrase("Madam, I'm Adam.");
+        assert(punctuatedPalindrome.letters(), "MadamImAdam");
+      });
+
+    });
+  });
+});
+
+              // TO CHECK IN REPL:
+              // let Phrase = require("./index.js");
+              // let palindrome = require("./index.js");
+              // let A = Phrase("A man, a plan, a canal - Panama!");
+              // $ A.palindrome();
+              // > true
+              // let B = Phrase("I am not a Palindrome.");
+              // $ B.palindrome();
+              // > false
